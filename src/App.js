6c69f9ab1b8ed5d3817cd20597e3478bc2e1c280
unclebusy <deniskalagin@gmail.com>
2024-08-28 11:11:26 +0500

    Stop tracking questionsMainCSS.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header } from './components/Header';
+import { Header } from './components/Header/Header';
 import { Home } from './pages/Home';
 import store from './redux/store';
 import { Provider } from 'react-redux';
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,41 @@
 import * as React from 'react';
+import styles from './Header.module.scss';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export function Header() {
   return (
-    <nav>
-      <img src="/images/logo.svg" />
-      <p>StepCess</p>
+    <nav className={styles.nav}>
+      <div className={styles.groupLogo}>
+        <img className={styles.logoImg} src="/images/logo.svg" />
+        <p className={styles.logoName}>StepCess</p>
+      </div>
+      <form>
+        <label>
+          <input type="radio" name="testType" value="frontend" />
+          Frontend
+        </label>
+        <label>
+          <input type="radio" name="testType" value="backend" />
+          Backend
+        </label>
+        <label>
+          <input type="radio" name="testType" value="fullstack" />
+          Fullstack
+        </label>
+      </form>
+      <div className={styles.groupAccount}>
+        <div>
+          <button></button>
+          <button></button>
+        </div>
+        <div>
+          <button></button>
+          <button></button>
+        </div>
+        <button>
+          <AccountCircleIcon className={styles.avatarIcon} />
+        </button>
+      </div>
     </nav>
   );
 }
